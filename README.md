c349ca3023f0b9039b0dd63d8ea5e12da07727b8
Chris Miller <chrisamiller@gmail.com>
2017-11-08 18:25:19 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker-copycat
+docker image for the R copyCat package
---
+++ rpackages.R
@@ -0,0 +1,2 @@
+source("https://bioconductor.org/biocLite.R")
+biocLite("methods, foreach, doMC, IRanges, DNAcopy, stringr")
